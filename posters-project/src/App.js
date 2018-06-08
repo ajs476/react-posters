e48a6ebdb879b35966a9af9ade54130573b72266
Alexander Sears <alexander.sears@moove-it.com>
2018-06-08 16:14:16 -0500

    created Poster component

--- posters-project/src/App.js
+++ posters-project/src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
+import Poster from './components/Poster.js';
 
 const App = () => (
 <div>
   <section className="container">
     <div className="row clearfix">
       <div className="column fifth">
-        <div className="media-item">
-          <img className="poster-image" src="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"></img>
-          <div className="under-poster">
-            <div className="under-poster-left">
-              <div className="media-title">
-                <h3>Deadpool</h3>
-              </div>
-              <div className="media-genre">
-                <h4>Action / Comedy</h4>
-              </div>
-            </div>
-            <div className="under-poster-right">
-              <img className="user-image" src="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"></img>
-            </div>
-          </div>
-        </div>
+        <Poster />
       </div>
       <div className="column fifth">
         <div className="media-item">
---
+++ posters-project/src/components/Poster.js
@@ -0,0 +1,22 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+
+const Poster = () => (
+  <div className="media-item">
+    <img className="poster-image" src="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"></img>
+    <div className="under-poster">
+      <div className="under-poster-left">
+        <div className="media-title">
+          <h3>Deadpool</h3>
+        </div>
+        <div className="media-genre">
+          <h4>Action / Comedy</h4>
+        </div>
+      </div>
+      <div className="under-poster-right">
+        <img className="user-image" src="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"></img>
+      </div>
+    </div>
+  </div>
+);
+
+export default Poster;
