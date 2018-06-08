f41cc044a744f257a4a96ea8f27102a395007b77
Alexander Sears <alexander.sears@moove-it.com>
2018-06-08 10:28:20 -0500

    initial react project setup with babel-eslint

---
+++ posters-project/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+
+const App = () => (
+  <div>
+    test!
+  </div>
+);
+
+export default App;
---
+++ posters-project/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import {render} from 'react-dom';
+import App from './App'; // eslint-disable-line no-unused-vars
+
+render(<App />, document.getElementById('root'));
