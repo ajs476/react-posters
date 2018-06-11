f5c50415b09389803af9a255f8cc6afb2461c27e
Alexander Sears <alexander.sears@moove-it.com>
2018-06-11 10:19:56 -0500

    generate posters via PosterRow, added poster-data.json

--- posters-project/src/App.js
+++ posters-project/src/App.js
@@ -1,72 +1,14 @@
 import React from 'react';
-import Poster from './components/Poster.js';
+import PosterRow from './components/PosterRow.js';
 
 const App = () => (
 <div>
   <section className="container">
     <div className="row clearfix">
-        <Poster
-          title="DeadPool"
-          genre="Action / Comedy"
-          posterImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-          userImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-        />
-        <Poster
-          title="DeadPool"
-          genre="Action / Comedy"
-          posterImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-          userImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-        />
-        <Poster
-          title="DeadPool"
-          genre="Action / Comedy"
-          posterImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-          userImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-        />
-        <Poster
-          title="DeadPool"
-          genre="Action / Comedy"
-          posterImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-          userImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-        />
-        <Poster
-          title="DeadPool"
-          genre="Action / Comedy"
-          posterImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-          userImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-        />
+        <PosterRow />
     </div>
     <div className="row clearfix">
-        <Poster
-          title="DeadPool"
-          genre="Action / Comedy"
-          posterImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-          userImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-        />
-        <Poster
-          title="DeadPool"
-          genre="Action / Comedy"
-          posterImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-          userImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-        />
-        <Poster
-          title="DeadPool"
-          genre="Action / Comedy"
-          posterImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-          userImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-        />
-        <Poster
-          title="DeadPool"
-          genre="Action / Comedy"
-          posterImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-          userImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-        />
-        <Poster
-          title="DeadPool"
-          genre="Action / Comedy"
-          posterImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-          userImageURL="https://i.pinimg.com/originals/1a/70/6e/1a706e0021180f77cf766c219319045f.jpg"
-        />
+        <PosterRow />
     </div>
   </section>
 </div>
