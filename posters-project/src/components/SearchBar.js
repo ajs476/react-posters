90f42935182880471646375b65f1706bf442285d
Alexander Sears <alexander.sears@moove-it.com>
2018-06-11 14:21:05 -0500

    added SearchBar component

--- posters-project/src/components/SearchBar.js
+++ posters-project/src/components/SearchBar.js
@@ -2,34 +2,60 @@ import React from 'react';
 const qhttp = require('q-io/http');
 
 /**
- * Adds two numbers together.
- * @param {int} num1 The first number.
- * @param {int} num2 The second number.
- * @return {text} The sum of the two numbers.
+ * Use text in the textbox to search movie details using omdb api.
+ * Called when movieSearchButton is clicked.
+ * @param {1} searchTitle provided title of the movie to search.
+*/
+function searchOMDB(searchTitle) {
+  qhttp.read(`http://www.omdbapi.com/?apikey=fd86ad97&t=${searchTitle}`)
+  .then(function(json) {
+    let omdbResult = JSON.parse(json);
+    let omdbResultTitle = omdbResult.Title;
+    let omdbResultGenre = omdbResult.Genre;
+    let omdbResultPoster = omdbResult.Poster;
+    alert(`Title: ${omdbResultTitle} | Genre: ${omdbResultGenre} | Poster: ${omdbResultPoster}`);
+  })
+  .then(null, Error)
+  .done();
+}
+
+/**
 */
 class SearchBar extends React.Component {
   /**
-  * Render a textbox.
-  * @return {input} return the textbox.
- */
+  */
+  constructor() {
+    super();
+    this.state = {
+      inputValue: '',
+    };
+  }
+  /**
+   * @param {1} changeEvent textbox change event
+  */
+  updateInputValue(changeEvent) {
+    this.setState({
+      inputValue: changeEvent.target.value,
+    });
+  }
+
+  /**
+   * Returns search box and button to page.
+   * @return {div} div containing the searchbar and search button
+  */
   render() {
     return (
       <div className="movieSearch">
-        <input className="movieSearchBox" type="text" placeholder="Search Movie on OMDB..."></input>
-        <button className="movieSearchButton">Search</button>
+        <input className="movieSearchBox" type="text" placeholder="Lookup Movie on OMDb..." value={this.state.inputValue}
+          onChange={ (changeEvent) =>
+            this.updateInputValue(changeEvent)
+          }>
+        </input>
+        <button className="movieSearchButton" onClick={() => searchOMDB(this.state.inputValue)}>Search</button>
       </div>
   );
   }
 }
 
-qhttp.read('http://www.omdbapi.com/?apikey=fd86ad97&t=guardians-of-the-galaxy')
-.then(function(json) {
-  let omdbResult = JSON.parse(json);
-  let omdbResultTitle = omdbResult.Title;
-  let omdbResultGenre = omdbResult.Genre;
-  console.log(`Title: ${omdbResultTitle} | Genre: ${omdbResultGenre}`);
-})
-.then(null, console.error)
-.done();
 
 export default SearchBar;
