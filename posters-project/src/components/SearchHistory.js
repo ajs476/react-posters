185e1cdfaa2e3e7da324b29868b63e188da7af36
Alexander Sears <alexander.sears@moove-it.com>
2018-06-13 16:21:52 -0500

    added ability to click history items and revist them

--- posters-project/src/components/SearchHistory.js
+++ posters-project/src/components/SearchHistory.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const SearchHistory = ({searchHistory, clearHistory}) => (
+const SearchHistory = ({searchHistory, clearHistory, visitHistory}) => (
   <div className="searchHistory">
-      <span>Previous Searches: </span>
-        {searchHistory}
+      <span>Search History: </span>
+        {searchHistory.map((item, index) =>
+           <span className='historyLink' key={index} onClick={() => visitHistory(item)}>{item}</span>)
+        }
       <button className="searchHistoryButton" value="Clear" onClick={clearHistory}>Clear</button>
   </div>
 );
