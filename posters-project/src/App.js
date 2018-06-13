f9cc2f91c59719141dcd71a7e57d034aa52a8058
Alexander Sears <alexander.sears@moove-it.com>
2018-06-13 09:59:33 -0500

    added error checking for api result, render error message in app

--- posters-project/src/App.js
+++ posters-project/src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
     this.state = {
       movies: posterData,
       inputValue: '',
+      statusMessage: '',
     };
     this.updateInputValue = this.updateInputValue.bind(this);
   }
@@ -27,21 +28,31 @@ class App extends React.Component {
   }
 
   searchOMDB(searchTitle) {
+    if (searchTitle == '') {
+      this.setState({statusMessage: 'Error: Search term required'});
+      return;
+    }
     qhttp.read(`http://www.omdbapi.com/?apikey=fd86ad97&s=${searchTitle}`)
     .then((results) => {
       let omdbResult = JSON.parse(results);
+      if (omdbResult.Response === 'False') {
+        this.setState({statusMessage: `Error: ${omdbResult.Error}`});
+        return;
+      }
       let formattedResults = omdbResult.Search.map(({Title: title, Year: genre, Poster: posterImageURL}) =>
         ({title, genre, posterImageURL, userImageURL: posterImageURL})
       );
       this.updateMovies(formattedResults);
+      this.setState({statusMessage: ''});
     })
-    .catch((error) => alert('Error: Something went wrong...'));
+    .catch((error) => this.setState({statusMessage: 'Error: Something went wrong. Please try again later.'}));
   }
 
   render() {
-    const {movies, inputValue} = this.state;
+    const {movies, inputValue, statusMessage} = this.state;
     return (
     <section className="container">
+      <p className="statusMessage">{statusMessage}</p>
       <SearchBar updateInputValue={this.updateInputValue} onSearch={() => this.searchOMDB(inputValue)} input={inputValue}/>
       <PosterList movieList={movies}/>
     </section>
