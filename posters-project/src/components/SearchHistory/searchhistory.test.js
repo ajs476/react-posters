24e19c71252cf8da3a1a7f3c3ef052f6661b6191
Alexander Sears <alexander.sears@moove-it.com>
2018-06-14 12:52:39 -0500

    testing searchHistory being cleared on clear button click

--- posters-project/src/components/SearchHistory/searchhistory.test.js
+++ posters-project/src/components/SearchHistory/searchhistory.test.js
@@ -15,3 +15,11 @@ it('calls onClick event on click of clear button', () =>{ // eslint-disable-line
   wrapper.find('button.searchHistoryButton').simulate('click');
   expect(onClick).toBeCalled(); // eslint-disable-line no-undef
 });
+
+it('search history is empty after clear button click', () =>{ // eslint-disable-line no-undef
+  const onClick = jest.fn(); // eslint-disable-line no-undef
+  let wrapper = mount(<SearchHistory searchHistory={testHistoryItems} clearHistory={onClick}/>);
+  wrapper.find('button.searchHistoryButton').simulate('click');
+  const afterClickText = wrapper.find('div.searchHistory').children().first().text();
+  expect(afterClickText).toEqual('Search History: '); // eslint-disable-line no-undef
+});
