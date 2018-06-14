4bbf5b9c3cb70973ec41b47574462410414fb168
Alexander Sears <alexander.sears@moove-it.com>
2018-06-14 13:39:08 -0500

    added jest to eslint, removed eslint-disable lines

--- posters-project/src/components/SearchHistory/searchhistory.test.js
+++ posters-project/src/components/SearchHistory/searchhistory.test.js
@@ -5,21 +5,21 @@ import {shallow, mount} from 'enzyme';
 let testHistoryItems = [['title', 'genre', 'posterImageURL', 'userImageURL'], ['title', 'genre', 'posterImageURL', 'userImageURL']];
 
 
-it('renders without crashing', () => { // eslint-disable-line no-undef
+it('renders without crashing', () => {
   shallow(<SearchHistory searchHistory={testHistoryItems}/>);
 });
 
-it('calls onClick event on click of clear button', () =>{ // eslint-disable-line no-undef
-  const onClick = jest.fn(); // eslint-disable-line no-undef
+it('calls onClick event on click of clear button', () =>{
+  const onClick = jest.fn();
   let wrapper = mount(<SearchHistory searchHistory={testHistoryItems} clearHistory={onClick}/>);
   wrapper.find('button.searchHistoryButton').simulate('click');
-  expect(onClick).toBeCalled(); // eslint-disable-line no-undef
+  expect(onClick).toBeCalled();
 });
 
-it('search history is empty after clear button click', () =>{ // eslint-disable-line no-undef
-  const onClick = jest.fn(); // eslint-disable-line no-undef
+it('search history is empty after clear button click', () =>{
+  const onClick = jest.fn();
   let wrapper = mount(<SearchHistory searchHistory={testHistoryItems} clearHistory={onClick}/>);
   wrapper.find('button.searchHistoryButton').simulate('click');
   const afterClickText = wrapper.find('div.searchHistory').children().first().text();
-  expect(afterClickText).toEqual('Search History: '); // eslint-disable-line no-undef
+  expect(afterClickText).toEqual('Search History: ');
 });
