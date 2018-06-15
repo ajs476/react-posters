7a5b49ec60f3cae3fedcf9ec711b3703e55d29e6
Alexander Sears <alexander.sears@moove-it.com>
2018-06-15 12:09:40 -0500

    added test for clearHistory method in App component

--- posters-project/src/app.test.js
+++ posters-project/src/app.test.js
@@ -5,3 +5,13 @@ import {shallow, mount} from 'enzyme';
 it('renders without crashing', () => {
   shallow(<App />);
 });
+
+it('searchHistory state is cleared when clearHistory method is called', () => {
+  let wrapper = mount(<App />);
+  let testHistoryState = {
+    searchHistory: ['history1', 'history2'],
+  };
+  wrapper.setState(testHistoryState);
+  wrapper.instance().clearHistory();
+  expect(wrapper.state('searchHistory')).toEqual([]);
+});
