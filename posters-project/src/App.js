934a6d2253da98d0ea1f0e70b7012de05610074b
Alexander Sears <alexander.sears@moove-it.com>
2018-06-15 12:31:50 -0500

    ignore coverage, remove unnecessary parameter in manageHistoryLength method

--- posters-project/src/App.js
+++ posters-project/src/App.js
@@ -28,14 +28,14 @@ class App extends React.Component {
     this.setState({searchHistory: []});
   }
 
-  manageHistoryLength(inputValue, searchHistory) {
+  manageHistoryLength(searchHistory) {
     if (searchHistory.length >= MAX_SEARCH_HISTORY) {
       searchHistory.pop();
     }
   }
 
   updateHistory(inputValue, searchHistory) {
-    this.manageHistoryLength(inputValue, searchHistory);
+    this.manageHistoryLength(searchHistory);
     this.setState({searchHistory: [`(${inputValue})`, ...searchHistory]});
   }
 
