d6987c6e8cee83652bdaa58a17fafd41d40e838f
Alexander Sears <alexander.sears@moove-it.com>
2018-06-15 09:42:56 -0500

    removed unnecessary test, added test for SearchBar onChange event

--- posters-project/src/components/SearchBar/searchbar.test.js
+++ posters-project/src/components/SearchBar/searchbar.test.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import SearchBar from './SearchBar.js';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 
-it('renders without crashing', () => { // eslint-disable-line no-undef
+it('renders without crashing', () => {
   shallow(<SearchBar />);
+});
+
+it('calls onChange event when searchBar is changed', () => {
+  const onChange = jest.fn();
+  let wrapper = mount(<SearchBar updateInputValue={onChange}/>);
+  wrapper.find('input.movieSearchBox').simulate('change');
+  expect(onChange).toBeCalled();
 });
--- posters-project/src/components/SearchHistory/searchhistory.test.js
+++ posters-project/src/components/SearchHistory/searchhistory.test.js
@@ -9,17 +9,9 @@ it('renders without crashing', () => {
   shallow(<SearchHistory searchHistory={testHistoryItems}/>);
 });
 
-it('calls onClick event on click of clear button', () =>{
+it('calls onClick event on click of clear button', () => {
   const onClick = jest.fn();
   let wrapper = mount(<SearchHistory searchHistory={testHistoryItems} clearHistory={onClick}/>);
   wrapper.find('button.searchHistoryButton').simulate('click');
   expect(onClick).toBeCalled();
-});
-
-it('search history is empty after clear button click', () =>{
-  const onClick = jest.fn();
-  let wrapper = mount(<SearchHistory searchHistory={testHistoryItems} clearHistory={onClick}/>);
-  wrapper.find('button.searchHistoryButton').simulate('click');
-  const afterClickText = wrapper.find('div.searchHistory').children().first().text();
-  expect(afterClickText).toEqual('Search History: ');
 });
